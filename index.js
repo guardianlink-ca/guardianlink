054ae6ea01aa57d41a2f4aa49a92a84e210cf7d2
Karter Man <karterman@Karters-MacBook-Pro.local>
2025-08-26 18:17:40 -0400

    Add /status endpoint

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-  res.json({ ok: true, app: 'GuardianLink running 🚀' });
+  res.json({ ok: true, app: 'GuardianLink running 🚀', version: 'v1' });
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
 
