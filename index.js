0209e1933a97cb7fd1903eb594226c31daaa3c2c
Karter Man <karterman@Karters-MacBook-Pro.local>
2025-08-26 14:52:36 -0400

    Initial GuardianLink server

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({ ok: true, app: 'GuardianLink running 🚀' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
